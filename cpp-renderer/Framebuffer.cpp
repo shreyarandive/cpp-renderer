e41fb99c863789c714c08081f50fed2fdc5bb238
Randive <srandive@purdue.edu>
2019-05-23 20:25:54 -0400

    Draw line segment

--- cpp-renderer/Framebuffer.cpp
+++ cpp-renderer/Framebuffer.cpp
@@ -7,19 +7,19 @@
 #include "Scene.h"
 #include "Vector3.h"
 
-FrameBuffer::FrameBuffer(int u0, int v0, int _w, int _h) : Fl_Gl_Window(u0, v0, _w, _h, nullptr) 
+Framebuffer::Framebuffer(int u0, int v0, int _w, int _h) : Fl_Gl_Window(u0, v0, _w, _h, nullptr) 
 {
 	w = _w;
 	h = _h;
 	pix = new unsigned int[w * h];
 }
 
-void FrameBuffer::draw()
+void Framebuffer::draw()
 {
 	glDrawPixels(w, h, GL_RGBA, GL_UNSIGNED_BYTE, pix);
 }
 
-int FrameBuffer::handle(int event)
+int Framebuffer::handle(int event)
 {
 	switch (event)
 	{
@@ -34,7 +34,7 @@ int FrameBuffer::handle(int event)
 	return 0;
 }
 
-void FrameBuffer::KeyboardHandle() 
+void Framebuffer::KeyboardHandle() 
 {
 	int key = Fl::event_key();
 
@@ -51,19 +51,24 @@ void FrameBuffer::KeyboardHandle()
 }
 
 
-void FrameBuffer::SetBGR(unsigned int bgr) 
+void Framebuffer::SetBGR(unsigned int bgr) 
 {
 	for (int uv = 0; uv < w * h; uv++)
 		pix[uv] = bgr;
 }
 
-void FrameBuffer::Set(int u, int v, unsigned int color)
+void Framebuffer::Set(int u, int v, unsigned int color)
 {
 	//pix[u * w + v] = color; //starts from bottom left corner of the screen
 	pix[(h - 1 - u) * w + v] = color;
 }
 
-void FrameBuffer::SetGuarded(int u, int v, unsigned int color)
+void Framebuffer::Set(Vector3 uv, unsigned int color)
+{
+	SetGuarded((int)uv[0], (int)uv[1], color);
+}
+
+void Framebuffer::SetGuarded(int u, int v, unsigned int color)
 {
 	if (u < 0 || v < 0 || u > h - 1 || v > w - 1)
 		return;
@@ -71,8 +76,23 @@ void FrameBuffer::SetGuarded(int u, int v, unsigned int color)
 	Set(u, v, color);
 }
 
+void Framebuffer::SetZB(Vector3 uvz, unsigned int color)
+{
+	int u = (int)uvz[0];
+	int v = (int)uvz[1];
+	if (u < 0 || v < 0 || u > w - 1 || v > h - 1)
+		return;
+
+	int uv = (h - 1 - v)*w + u;
+	if (zb[uv] > uvz[2])
+		return;
+	zb[uv] = uvz[2];
+
+	Set(uvz, color);
+}
+
 // load a tiff image to pixel buffer
-void FrameBuffer::LoadTiff(char* fname)
+void Framebuffer::LoadTiff(char* fname)
 {
 	TIFF* in = TIFFOpen(fname, "r");
 	if (in == nullptr) 
@@ -104,7 +124,7 @@ void FrameBuffer::LoadTiff(char* fname)
 }
 
 // save as tiff image
-void FrameBuffer::SaveAsTiff(char *fname) 
+void Framebuffer::SaveAsTiff(char *fname) 
 {
 
 	TIFF* out = TIFFOpen(fname, "w");
@@ -131,14 +151,14 @@ void FrameBuffer::SaveAsTiff(char *fname)
 	TIFFClose(out);
 }
 
-void FrameBuffer::DrawRectangle(int u0, int v0, int u1, int v1, unsigned int color)
+void Framebuffer::DrawRectangle(int u0, int v0, int u1, int v1, unsigned int color)
 {
 	for(int i = u0 ; i <= u1 ; i++)
 		for (int j = v0; j <= v1; j++)
 			SetGuarded(i, j, color);
 }
 
-void FrameBuffer::DrawCircle(int u0, int v0, int radius, unsigned int color)
+void Framebuffer::DrawCircle(int u0, int v0, int radius, unsigned int color)
 {
 	int u1 = u0 - radius;
 	int u2 = u0 + radius;
@@ -149,5 +169,26 @@ void FrameBuffer::DrawCircle(int u0, int v0, int radius, unsigned int color)
 		for(int j = v1 ; j <= v2 ; j++)
 			if(((i - u0) * (i - u0) + (j - v0) * (j - v0)) <= radius * radius)
 				SetGuarded(i, j, color);
+}
+
+void Framebuffer::DrawLineSegment(Vector3 p0, Vector3 p1, Vector3 c0, Vector3 c1) 
+{
+	float du = fabsf(p0[0] - p1[0]);
+	float dv = fabsf(p0[1] - p1[1]);
+	int stepsN;
+	
+	if (du > dv) 
+		stepsN = static_cast<int>(du + 2);
+	else 
+		stepsN = static_cast<int>(dv + 2);
+
+	Vector3 duv = (p1 - p0) / (float)(stepsN - 1);
+	Vector3 dc = (c1 - c0) / (float)(stepsN - 1);
+	for (int stepi = 0; stepi < stepsN; stepi++) 
+	{
+		Vector3 currp = p0 + duv * (float)stepi;
+		Vector3 currc = c0 + dc * (float)stepi;
+		SetZB(currp, currc.GetColor());
+	}
 }
 
--- cpp-renderer/Framebuffer.h
+++ cpp-renderer/Framebuffer.h
@@ -6,7 +6,7 @@
 
 #include "Vector3.h"
 
-class FrameBuffer : public Fl_Gl_Window 
+class Framebuffer : public Fl_Gl_Window 
 {
 private:
 	void Set(int u, int v, unsigned int color);
@@ -14,16 +14,23 @@ private:
 public:
 	unsigned int *pix;
 	int w, h;
-	FrameBuffer(int u0, int v0, int _w, int _h);
+	float *zb;
+
+	Framebuffer(int u0, int v0, int _w, int _h);
+
+	int handle(int guievent);
+
 	void draw();
 	void KeyboardHandle();
-	int handle(int guievent);
 	void SetBGR(unsigned int bgr);
+	void Set(Vector3 v, unsigned int color);
 	void SetGuarded(int u, int v, unsigned int color);
+	void SetZB(Vector3 uvz, unsigned int color);
 	void LoadTiff(char* fname);
 	void SaveAsTiff(char* fname);
 	
 	//draw shapes
 	void DrawRectangle(int u0, int v0, int u1, int v1, unsigned int color);
 	void DrawCircle(int u0, int v0, int radius, unsigned int color);
+	void DrawLineSegment(Vector3 p0, Vector3 p1, Vector3 c0, Vector3 c1);
 };
